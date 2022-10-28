8ca26b09cdb95073a34fcc2da4140dc4750ad950
duccanhole <duccanhole@gmail.com>
2022-10-28 14:18:38 +0700

    erge code

--- lib/route/home-widget.dart
+++ lib/route/home-widget.dart
@@ -1,10 +1,8 @@
-import 'package:app/route/footer-bar.dart';
+import 'package:app/route/SignInPage.dart';
 import 'package:app/route/header-bar.dart';
-import 'package:app/route/statistic.dart';
 import 'package:app/route/test-area.dart';
 import 'package:flutter/material.dart';
 
-import 'index.dart';
 
 class HomeWidget extends StatelessWidget {
   const HomeWidget({Key? key}) : super(key: key);
@@ -19,36 +17,7 @@ class HomeWidget extends StatelessWidget {
       //   '/': (context) => const IndexPage(),
       //   '/statistic': (context) => const Statistic()
       // },
-      home: const DScaffold(),
-    );
-  }
-}
-
-class DScaffold extends StatefulWidget {
-  const DScaffold({super.key});
-
-  @override
-  State<StatefulWidget> createState() => DScaffoldState();
-}
-
-class DScaffoldState extends State<DScaffold> {
-  int currIndex = 0;
-  navigateRoute(int index) {
-    setState(() {
-      currIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: const HeaderBar(),
-      body: IndexedStack(
-        index: currIndex,
-        children: const [IndexPage(), Statistic(), TestArea()],
-      ),
-      bottomNavigationBar:
-          FooterBar(navigateRoute: navigateRoute, index: currIndex),
+      home: const SignInPage(),
     );
   }
 }
