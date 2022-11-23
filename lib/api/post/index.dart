4ed374e9700a9909b8bc67033d4cd516d14592aa
duccanhole <duccanhole@gmail.com>
2022-11-23 22:22:51 +0700

    add action create post

--- lib/api/post/index.dart
+++ lib/api/post/index.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:app/api/index.dart';
 import 'package:app/interface/Post.interface.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class QuerySearch {
   String skip;
@@ -14,7 +15,7 @@ class QuerySearch {
 class PostApi {
   Future<List<IPost>> getList(QuerySearch q) async {
     final query = {"skip": q.skip, "limit": q.limit, "sortBy": q.sortBy};
-    final uri = Uri.https(rootUrl, "post", query);
+    final uri = Uri.http(rootUrl, "post", query);
     final response = await http.get(uri);
     if (response.statusCode == 200) {
       // If the server did return a 200 OK response,
@@ -26,5 +27,31 @@ class PostApi {
       // then throw an exception.
       throw Exception('Failed to load from server');
     }
+  }
+
+  Future<http.Response?> createPost(
+      String title, String subtitle, String tags, String link) async {
+    final prefs = await SharedPreferences.getInstance();
+    String? userId = prefs.getString("userId");
+    String? token = prefs.getString("token");
+    if (userId!.isNotEmpty && token!.isNotEmpty) {
+      try {
+        return http.post(Uri.http(rootUrl, "post/create"),
+            headers: <String, String>{
+              'Content-Type': 'application/json; charset=UTF-8',
+              'token': token
+            },
+            body: json.encode({
+              'title': title,
+              'subtitle': subtitle,
+              'url': link,
+              'tags': tags,
+              'userId': userId
+            }));
+      } catch (e) {
+        throw Exception(e);
+      }
+    }
+    return null;
   }
 }
--- lib/route/PostEdit.dart
+++ lib/route/PostEdit.dart
@@ -1,10 +1,22 @@
+import 'dart:convert';
+import 'dart:ui';
+
+import 'package:app/api/post/index.dart';
 import 'package:app/interface/Post.interface.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 class PostEditPage extends StatefulWidget {
-  const PostEditPage({super.key, this.isEdit = false});
+  const PostEditPage(
+      {super.key,
+      this.isEdit = false,
+      this.id = '',
+      this.title = '',
+      this.subtitle = '',
+      this.url = '',
+      this.tags = ''});
   final bool isEdit;
+  final String id, title, subtitle, url, tags;
 
   @override
   State<PostEditPage> createState() => PostEditPageState();
@@ -20,9 +32,60 @@ class PostEditPageState extends State<PostEditPage> {
     'Entertaiment',
     'Other'
   ];
+  InputDecoration inputStyle(String labelText) {
+    return InputDecoration(
+      labelText: labelText,
+      labelStyle: const TextStyle(color: Colors.blueGrey),
+      enabledBorder: const OutlineInputBorder(
+        borderSide: BorderSide(color: Colors.blueGrey),
+      ),
+      border: const OutlineInputBorder(),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
+    TextEditingController postTitle = TextEditingController(text: widget.title),
+        postSubtitle = TextEditingController(text: widget.subtitle),
+        postUrl = TextEditingController(text: widget.url);
+    String postTags = widget.tags == '' ? "Other" : widget.tags;
+    bool loading = false;
+
+    bool checkRequire(String value) {
+      return !(value.trim() == "");
+    }
+
+    bool checkUrl(String value) {
+      return Uri.parse(value).isAbsolute;
+    }
+
+    void goBack() {
+      Navigator.pop(context);
+    }
+
+    void showMessage(String mess) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(mess)));
+    }
+
+    void onSave() async {
+      if (formKey.currentState!.validate()) {
+        setState(() {
+          loading = true;
+        });
+        Response? res = await PostApi().createPost(
+            postTitle.text, postSubtitle.text, postTags, postUrl.text);
+        if (res!.statusCode >= 200 && res.statusCode <= 299) {
+          showMessage("Success");
+          goBack();
+        } else {
+          showMessage("Error, please try again.");
+        }
+        setState(() {
+          loading = false;
+        });
+      }
+    }
+
     return Scaffold(
         backgroundColor: Theme.of(context).backgroundColor,
         body: Padding(
@@ -30,64 +93,85 @@ class PostEditPageState extends State<PostEditPage> {
           child: Center(
               child: Column(
             children: [
+              Text(widget.isEdit ? 'Edit post' : 'Create Post',
+                  style: const TextStyle(color: Colors.blueGrey, fontSize: 24)),
+              const SizedBox(height: 15),
               Form(
                   key: formKey,
                   child: Column(
                     children: [
                       TextFormField(
+                        controller: postTitle,
                         style: const TextStyle(color: Colors.blueGrey),
-                        decoration: const InputDecoration(
-                          labelText: "Title*",
-                          labelStyle: TextStyle(color: Colors.blueGrey),
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.blueGrey),
-                          ),
-                          border: OutlineInputBorder(),
-                        ),
+                        decoration: inputStyle('Title*'),
+                        validator: (value) {
+                          if (!checkRequire(value!)) return 'Required';
+                          return null;
+                        },
                       ),
                       const SizedBox(height: 15),
                       TextFormField(
+                        controller: postSubtitle,
                         style: const TextStyle(color: Colors.blueGrey),
-                        decoration: const InputDecoration(
-                          labelText: "Subtitle",
-                          labelStyle: TextStyle(color: Colors.blueGrey),
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.blueGrey),
-                          ),
-                          border: OutlineInputBorder(),
-                        ),
+                        decoration: inputStyle('Subtitle'),
                       ),
                       const SizedBox(height: 15),
                       TextFormField(
+                        controller: postUrl,
                         style: const TextStyle(color: Colors.blueGrey),
-                        decoration: const InputDecoration(
-                          labelText: "Link",
-                          labelStyle: TextStyle(color: Colors.blueGrey),
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.blueGrey),
-                          ),
-                          border: OutlineInputBorder(),
-                        ),
+                        decoration: inputStyle('Link*'),
+                        validator: (value) {
+                          if (!checkRequire(value!)) return 'Required';
+                          if (!checkUrl(value)) return 'Link is invalid';
+                          return null;
+                        },
                       ),
                       const SizedBox(height: 15),
                       DropdownButtonFormField(
-                          style: const TextStyle(color: Colors.black),
-                          decoration: const InputDecoration(
-                              labelText: "Tags",
-                              labelStyle: TextStyle(color: Colors.blueGrey),
-                              enabledBorder: OutlineInputBorder(
-                                borderSide: BorderSide(color: Colors.blueGrey),
-                              ),
-                              border: OutlineInputBorder()),
+                          value: postTags,
+                          style: const TextStyle(color: Colors.blueGrey),
+                          decoration: inputStyle('Tags*'),
                           isExpanded: true,
                           items: tagsList
                               .map<DropdownMenuItem<String>>((e) =>
                                   DropdownMenuItem(value: e, child: Text(e)))
                               .toList(),
-                          onChanged: ((value) {})),
+                          onChanged: (String? value) {
+                            postTags = value!.toString();
+                          }),
+                      const SizedBox(height: 15),
                       Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          
+                          ElevatedButton(
+                              style: ButtonStyle(
+                                  backgroundColor:
+                                      MaterialStateColor.resolveWith(
+                                          (states) => Colors.grey)),
+                              onPressed: goBack,
+                              child: const Text('Cancel')),
+                          Row(
+                            children: [
+                              if (widget.isEdit)
+                                ElevatedButton(
+                                    style: ButtonStyle(
+                                        backgroundColor:
+                                            MaterialStateColor.resolveWith(
+                                                (states) => Colors.red)),
+                                    onPressed: () {},
+                                    child: const Text('Delete')),
+                              ElevatedButton(
+                                  style: ButtonStyle(
+                                      backgroundColor:
+                                          MaterialStateColor.resolveWith(
+                                              (states) => widget.isEdit
+                                                  ? Colors.blue
+                                                  : Colors.green)),
+                                  onPressed: loading ? null : onSave,
+                                  child:
+                                      Text(widget.isEdit ? 'Save' : 'Create'))
+                            ],
+                          )
                         ],
                       )
                     ],
